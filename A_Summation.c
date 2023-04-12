b54eb370763cffe483a722bb7a971f8a6c7fbc54
Mizanur Rahman <mizan.prog@gmail.com>
2023-04-13 00:12:16 +0600

    solve-summation

--- A_Summation.c
+++ A_Summation.c
@@ -1,5 +1,20 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main(){
-    
+    int n;
+    scanf("%d",&n);
+    long long arr[n];
+    long long sum=0;
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%lld",&arr[i]);
+    }
+    for(int j=0; j<n;j++){
+        sum +=arr[j];
+    }
+    if(sum<0){
+        sum=sum*-1;
+    }
+    printf("%lld",sum);
     return 0;
 }
