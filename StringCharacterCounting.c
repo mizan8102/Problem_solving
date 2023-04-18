70f58837e738f357ceafd59a595763d549049a13
Mizanur Rahman <mizan.prog@gmail.com>
2023-04-18 21:32:43 +0600

    character-count-from-string

---
+++ StringCharacterCounting.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char name[100];
+    scanf("%s",name);
+    int cnt[26]={0};
+    for (int i = 0; i < strlen(name); i++)
+    {
+        int a=name[i]-97;
+        cnt[a]++;
+    }
+    for (int i = 0; i < 26; i++)
+    {
+        if(cnt[i] !=0){
+            printf("%c-%d\n",i+97,cnt[i]);
+        }
+    }
+    return 0;
+}
