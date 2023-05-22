ff8a6dfbe29d361cdfe9aa59a24db4ada799cbbb
Mizanur Rahman <mizan.prog@gmail.com>
2023-05-22 19:21:35 +0600

    up

---
+++ DSA_for_C++/dynamic_array_size_increase_delete.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+#include<iostream>
+using namespace std;
+
+int main(){
+   int *ar=new int[5];
+   int *b=new int[7];
+   for(int i=0; i<5; i++)
+        cin >> ar[i];
+    for (int i = 0; i < 7; i++)
+    {
+         b[i]=ar[i];
+    }
+    b[5]=90;
+    b[6]=34;
+    for (int i = 0; i < 7; i++)
+    {
+        cout << b[i] << " ";
+    }
+    delete[] ar;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << ar[i] << " ";
+    }
+    
+   return 0;
+}
