7ee17553a68009da7065d7c5967aaabe4ef0ada4
Mizanur Rahman <mizan.prog@gmail.com>
2023-05-22 19:10:09 +0600

    up

---
+++ DSA_for_C++/HeapMemory.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+
+#include<iostream>
+using namespace std;
+
+int main(){
+   int *a=new int; // new for dynamic memory Heap 
+   *a=34;
+   cout << *a << endl;
+   return 0;
+}
