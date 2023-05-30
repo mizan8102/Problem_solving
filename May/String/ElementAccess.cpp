5ba0bfbd779196f6e02a167b7df498a02c542ddd
Mizanur Rahman <mizan.prog@gmail.com>
2023-05-30 22:09:30 +0600

    uploading-data

---
+++ May/String/ElementAccess.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+#include<iostream>
+using namespace std;
+
+int main(){
+   
+   string name="Mizanur RahmaD";
+   cout << name[0] << endl;
+   // at function 
+   cout << name.at(0) << endl;
+   cout << name.back() << endl;
+   return 0;
+}
