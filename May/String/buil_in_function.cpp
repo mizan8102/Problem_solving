2082a097f224e746bf403dc0e653d7a3290f8606
Mizanur Rahman <mizan.prog@gmail.com>
2023-05-30 21:50:20 +0600

    uploa

---
+++ May/String/buil_in_function.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+#include<iostream>
+using namespace std;
+
+int main(){
+   string a="Mizanur rAhman";
+   cout << "Size a string =" <<  a.size() << endl;
+
+   cout << "capacity of a string =" << a.capacity() << endl;
+   cout << "define max size=" << a.max_size() << endl;  
+   return 0;
+}
