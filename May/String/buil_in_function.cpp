d5f04b49f93bff2a362ab1f6313d4611d91e3de7
Mizanur Rahman <mizan.prog@gmail.com>
2023-05-30 21:52:19 +0600

    function-clear

--- May/String/buil_in_function.cpp
+++ May/String/buil_in_function.cpp
@@ -8,6 +8,8 @@ int main(){
    cout << "Size a string =" <<  a.size() << endl;
 
    cout << "capacity of a string =" << a.capacity() << endl;
-   cout << "define max size=" << a.max_size() << endl;  
+   cout << "define max size=" << a.max_size() << endl;
+   a.clear();
+   cout << "After clear value of a=" << a << endl;  
    return 0;
 }
