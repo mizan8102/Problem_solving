4fd30d754d7a8243795ef8677172cfd32bfd5411
Mizanur Rahman <mizan.prog@gmail.com>
2023-05-30 22:10:45 +0600

    access-front

--- May/String/ElementAccess.cpp
+++ May/String/ElementAccess.cpp
@@ -9,6 +9,9 @@ int main(){
    cout << name[0] << endl;
    // at function 
    cout << name.at(0) << endl;
+   // find last element
    cout << name.back() << endl;
+   // access the first element 
+   cout << name.front() << endl;
    return 0;
 }
